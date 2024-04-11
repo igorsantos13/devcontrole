c0835e25e7e5e01bcc8d78411d4720d6bd03a8b6
Igor Santos <igorsantos9513@outlook.com>
2024-04-11 15:40:44 -0300

    feat: home

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "@/components/container";
+
+function Dashboard() {
+  return (
+    <Container>
+      <h1>Página dashboard</h1>
+    </Container>
+  );
+}
+
+export default Dashboard;
