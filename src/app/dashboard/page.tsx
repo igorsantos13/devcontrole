79f0f858defc3505ecc7126ec28e2e226bed7ff0
Igor Santos <igorsantos9513@outlook.com>
2024-04-13 13:27:39 -0300

    only logged users can access dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { Container } from "@/components/container";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
 
-function Dashboard() {
+async function Dashboard() {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    redirect("/");
+  }
+
   return (
     <Container>
       <h1>Página dashboard</h1>
